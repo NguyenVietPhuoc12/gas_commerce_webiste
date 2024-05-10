2c18d9e159ea63a3da25efdd863032128aa5bce2
NguyenVietPhuoc12 <nguyenvietphuoc24.12@gmail.com>
2024-05-10 10:55:57 +0700

    Build default layout for project & using classnames for set named className jsx

--- src/layouts/DefaultLayout/Sidebar/index.jsx
+++ src/layouts/DefaultLayout/Sidebar/index.jsx
@@ -1,6 +1,11 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 const Sidebar = () => {
     return (
-        <div>
+        <div className={cx('sidebar-wrapper')}>
             <h2>Sidebar content</h2>
         </div>
     );
--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -1,5 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 const Header = () => {
-    return <h2>Header content</h2>;
+    return (
+        <header className={cx('header-wrapper')}>
+            <div className={cx('header-content')}>
+                <h2>Header content</h2>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
