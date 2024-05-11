b8a93df654d76b1f3d8d3964e794a373efdfa5b4
NguyenVietPhuoc12 <nguyenvietphuoc24.12@gmail.com>
2024-05-11 14:25:12 +0700

    Build UI search input is a past of header component

--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -1,3 +1,8 @@
+import { Link } from 'react-router-dom';
+
+import { IoIosCloseCircle } from 'react-icons/io';
+import { IoSearch } from 'react-icons/io5';
+
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 
@@ -7,7 +12,35 @@ const Header = () => {
     return (
         <header className={cx('header-wrapper')}>
             <div className={cx('header-content')}>
-                <h2>Header content</h2>
+                {/*------------------------ Logo container jsx--------------------- */}
+                <Link to={'/'}>
+                    <div className={cx('header__content-logo')}>
+                        <img src={require('~/assets/images/logo.png')} alt="logo" />
+                        <div className={cx('header__content-title')}>
+                            <p>Cửa hàng dầu khí hoá lỏng</p>
+                            <h3>Phước Thịnh</h3>
+                        </div>
+                    </div>
+                </Link>
+                {/*-------------X---------- Logo container jsx-----------X--------- */}
+
+                {/*------------------------ Search container jsx--------------------- */}
+                <div className={cx('header-search')}>
+                    <input type="text" placeholder="Tìm kiếm sản phẩm..." spellCheck={false} />
+                    <button className={cx('header-search-clear')}>
+                        <IoIosCloseCircle className={cx('icon')} />
+                    </button>
+                    <button className={cx('header-search-btn')}>
+                        <IoSearch className={cx('icon')} />
+                    </button>
+                </div>
+                {/*-------------X---------- Search container jsx------------X-------- */}
+
+                {/*------------------------ Actions container jsx--------------------- */}
+                <div className={cx('header-actions')}>
+                    <h2>Actions container</h2>
+                </div>
+                {/*-------------X---------- Actions container jsx------------X-------- */}
             </div>
         </header>
     );
